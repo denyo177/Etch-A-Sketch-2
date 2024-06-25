748749fd5da125d12bd92ec8e6d0b35a468fb5e4
Dennis <121116755+denyo177@users.noreply.github.com>
2024-06-25 14:56:44 +0200

    Finish function drawGrid that draws grid of square divs

--- script.js
+++ script.js
@@ -3,18 +3,20 @@
 function drawGrid(n) {
 
     const container = document.querySelector(".container");
-    container.style.display = "flex";
-    // container.style.boxSizing = "border-box"; 
 
-    for(i = 0; i < n; i++) {
-        const row = document.createElement("div");
-        row.classList.add("row");
-        row.setAttribute("style", "border: 1px solid black");
-        row.style.height = "35px";
-        row.style.width = "35px";
-        row.style.boxSizing = "border-box"; 
-        container.appendChild(row);
-
+    for(j = 0; j < n; j++) {
+        const column = document.createElement("div");
+        column.style.display = "flex";
+        for(i = 0; i < n; i++) {
+            const row = document.createElement("div");
+            row.classList.add("row");
+            row.setAttribute("style", "border: 1px solid black");
+            row.style.height = "35px";
+            row.style.width = "35px";
+            row.style.boxSizing = "border-box"; 
+            column.appendChild(row);
+        }
+        container.appendChild(column);
     }
    
 }
